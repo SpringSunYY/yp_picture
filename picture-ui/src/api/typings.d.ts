911e15d1e347545260ef47c0fa06fa94cd09413f
SpringSunYY <3217778348@qq.com>
2025-02-16 17:22:35 +0800

    图片扩展功能-批量修改图片

--- picture-ui/src/api/typings.d.ts
+++ picture-ui/src/api/typings.d.ts
@@ -230,6 +230,14 @@ declare namespace API {
     userId?: number
   }
 
+  type PictureEditByBatchRequest = {
+    category?: string
+    nameRule?: string
+    pictureIdList?: number[]
+    spaceId?: number
+    tags?: string[]
+  }
+
   type PictureEditRequest = {
     category?: string
     id?: number
